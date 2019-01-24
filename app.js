50570b3d7a11be4b48bdcad3a3d53dca3058cc7a
playbook <playbook@ue-macbook-13-share.local>
2019-01-24 14:25:00 +0100

    Bugfixes

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 if (window.attachEvent) {
     window.attachEvent("onload", reset);
+    var lost = false;
   } else if (window.addEventListener) {
     window.addEventListener("load", reset, false);
+    var lost = false;
   } else {
     document.addEventListener("load", reset, false);
+    var lost = false;
   }
   
   const history = document.querySelector(".history");
@@ -12,9 +15,12 @@ if (window.attachEvent) {
   const input = document.querySelector(".input");
   const button = document.querySelector(".button");
   const amount = document.getElementById("amount");
+  const submit = document.getElementById("submit");
   
   var random = Math.floor(Math.random() * document.getElementById("amount").value)+1;
   var turn = 0;
+  
+  submit.addEventListener("click", check());
   
   amount.addEventListener("change", function() {
     random = Math.floor(Math.random() * document.getElementById("amount").value)+1;
@@ -53,50 +59,54 @@ if (window.attachEvent) {
     }
   });
   
-  button.onclick = check();
   
+  
+if (lost == false) { 
   function check() {
-    var guess = parseInt(document.getElementById("input").value);
-    if (
-      guess > -1 &&
-      guess < parseInt(document.getElementById("amount").value) + 1
-    ) {
-      if (turn == 0) {
-        history.textContent = "Versuche ";
-      }
-      history.textContent += guess + " ";
-      if (guess == random) {
-        result.textContent = "RICHTIG";
-        document.getElementById("result").style.backgroundColor = "green";
-        low_high.textContent = "Du hast gewonnen.";
-        gameOver();
-      } else if (turn < parseInt(document.getElementById("difficulty").value)) {
-        result.textContent = "FALSCH";
-        document.getElementById("result").style.backgroundColor = "red";
-        if (guess > random) {
-          low_high.textContent = "zu hoch";
-        } else {
-          low_high.textContent = "zu niedrig";
+      var guess = parseInt(document.getElementById("input").value);
+      if (
+        guess > -1 &&
+        guess < parseInt(document.getElementById("amount").value) + 1
+      ) {
+        if (turn == 0) {
+          history.textContent = "Versuche ";
         }
+        history.textContent += guess + " ";
+        if (guess == random) {
+          result.textContent = "RICHTIG";
+          document.getElementById("result").style.backgroundColor = "green";
+          low_high.textContent = "Du hast gewonnen.";
+          gameOver();
+        } else if (turn < parseInt(document.getElementById("difficulty").value)-1) {
+          result.textContent = "FALSCH";
+          document.getElementById("result").style.backgroundColor = "red";
+          if (guess > random) {
+            low_high.textContent = "zu hoch";
+          } else {
+            low_high.textContent = "zu niedrig";
+          }
+        } else {
+          result.textContent = "Du hast verloren. n00b.";
+          gameOver();
+        }
+        turn++;
+        document.getElementById("input").value = "";
       } else {
-        result.textContent = "Du hast verloren. n00b.";
-        gameOver();
+        document.getElementById("input").value = "";
+        low_high.textContent = "Fehler";
+        result.textContent = `Gebe eine Zahl zwischen 1 und ${
+          document.getElementById("amount").value
+        } ein.`;
+        document.getElementById("result").style.backgroundColor = "#e0af02";
       }
-      turn++;
-      document.getElementById("input").value = "";
-    } else {
-      document.getElementById("input").value = "";
-      low_high.textContent = "Fehler";
-      result.textContent = `Gebe eine Zahl zwischen 1 und ${
-        document.getElementById("amount").value
-      } ein.`;
-      document.getElementById("result").style.backgroundColor = "#e0af02";
     }
   }
   
   function gameOver() {
     input.setContent = "";
     document.getElementById("input").value = "";
+    lost = true;
+    turn = 0;
   }
   
   function reset() {
@@ -104,6 +114,7 @@ if (window.attachEvent) {
     low_high.textContent = "";
     result.textContent = "";
     input.setContent = "";
-    turn = 1;
     document.getElementById("input").value = "";
+    lost = false;
+    random = Math.floor(Math.random() * document.getElementById("amount").value)+1;
   }
