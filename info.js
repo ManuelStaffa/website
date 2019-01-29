e241c7d3869a29bba4d0b0d1ea4246089df15cc0
playbook <playbook@ue-macbook-13-share.local>
2019-01-29 12:42:29 +0100

    use fetch to get information

--- info.js
+++ info.js
@@ -1,76 +1,51 @@
 //InfoPanel
-function ajax_get(url, callback) {
-  var json = new XMLHttpRequest();
-  json.onreadystatechange = function() {
-    if (json.readyState == 4 && json.status == 200) {
-      console.log("responseText:" + json.responseText);
-      try {
-        var data = JSON.parse(json.responseText);
-      } catch (err) {
-        console.log(err.message + " in " + json.responseText);
-        return;
-      }
-      callback(data);
-    }
-  };
-  json.open("GET", url, true);
-  json.send();
-}
 
-ajax_get("https://api.spacexdata.com/v3/launches/next", function(data) {
-  console.log(data);
-  var html = "";
-  debugger;
-  html += "<h2>" + "Next SpaceX Launch: " + data.mission_name + "</h2>";
-  html += `<br><p><strong> Rocket Name <br></strong> ${
-    data.rocket.rocket_name
-  } </p>`;
-  html += `<p><strong> Payload <br></strong> ${
-    data.rocket.second_stage.payloads[0].payload_id
-  } <br>
-     from ${data.rocket.second_stage.payloads[0].customers} <br>
-     in ${data.rocket.second_stage.payloads[0].nationality} </p>`;
-  html += `<p><strong> Flight Number <br></strong> ${data.flight_number} </p>`;
-  html += `<p><strong> Launch Year <br></strong> ${data.launch_year} </p>`;
-  html += `<p><strong> Launch Date <br></strong> ${
-    data.launch_date_local
-  } </p>`;
-  html += `<p><strong> Launch Site <br></strong> ${
-    data.launch_site.site_name_long
-  } 
-    (${data.launch_site.site_name}) </p>`;
+fetch("https://api.spacexdata.com/v3/launches/next")
 
-  document.getElementById("content").innerHTML = html;
+    .then(function(response) {
+        return response.json();
+    })
 
-  var launchDate = new Date(data.launch_date_local).getTime();
+    .then(function(json) {
+        var html = `
+        <h2> Next SpaceX Launch: ${json.mission_name} </h2> <br>
+        <p><strong> Rocket Name <br></strong> ${json.rocket.rocket_name}</p>
+        <p><strong> Payload <br></strong> ${json.rocket.second_stage.payloads[0].payload_id} <br>
+            from ${json.rocket.second_stage.payloads[0].customers} <br>
+            in ${json.rocket.second_stage.payloads[0].nationality} </p>
+        <p><strong> Flight Number <br></strong> ${json.flight_number} </p>
+        <p><strong> Launch Year <br></strong> ${json.launch_year} </p>
+        <p><strong> Launch Date <br></strong> ${json.launch_date_local}</p>
+        <p><strong> Launch Site <br></strong> ${json.launch_site.site_name_long} (${json.launch_site.site_name}) </p>  
+        `;
 
-  var temp = setInterval(function() {
-    var currentDate = new Date().getTime();
+        console.log(html);
+        document.getElementById("content").innerHTML = html;
 
-    var timeSpan = launchDate - currentDate;
+        var launchDate = new Date(json.launch_date_local).getTime();
 
-    var days = Math.floor(timeSpan / (1000 * 60 * 60 * 24));
-    var hours = Math.floor(
-      (timeSpan % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    var minutes = Math.floor((timeSpan % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((timeSpan % (1000 * 60)) / 1000);
+        var temp = setInterval(function() {
+            var currentDate = new Date().getTime();
 
-    document.getElementById("timer").innerHTML =
-      "<h2>" +
-      days +
-      "d " +
-      hours +
-      "h " +
-      minutes +
-      "m " +
-      seconds +
-      "s " +
-      "</h2>";
+            var timeSpan = launchDate - currentDate;
 
-    if (timeSpan < 0) {
-      clearInterval(temp);
-      document.getElementById("timer").innerHTML = "EXPIRED";
-    }
-  }, 1000);
-});
+            var days = Math.floor(timeSpan / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((timeSpan % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((timeSpan % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((timeSpan % (1000 * 60)) / 1000);
+
+            document.getElementById("timer").innerHTML = `
+            <h2> ${days}d ${hours}h ${minutes}m ${seconds}s </h2>
+            `;
+
+            if (timeSpan < 0) {
+            clearInterval(temp);
+            document.getElementById("timer").innerHTML = "EXPIRED";
+            }
+        }, 1000);
+    })
+
+    .catch(function(error) {
+        console.log(`fetch failed: `, error);
+    })
+    
